51bc3632608f507de952f7b4e5713745f2b4a5cd
Fábio Relvas <faabio.relvas@hotmail.com>
2019-10-01 19:02:03 -0300

    Updates

---
+++ precache-manifest.05fe828e96b6566cc07ff2cc2093f437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d345bae6a962c7d27fe560879f7e07b7",
+    "url": "/FabioRelvas/projeto-imobiliaria-crud/index.html"
+  },
+  {
+    "revision": "024d0f3e72f4ba80dd5a",
+    "url": "/FabioRelvas/projeto-imobiliaria-crud/static/css/2.15315edc.chunk.css"
+  },
+  {
+    "revision": "1e29ef02ad48f238f818",
+    "url": "/FabioRelvas/projeto-imobiliaria-crud/static/css/main.64a05b3c.chunk.css"
+  },
+  {
+    "revision": "024d0f3e72f4ba80dd5a",
+    "url": "/FabioRelvas/projeto-imobiliaria-crud/static/js/2.aa6b8e9e.chunk.js"
+  },
+  {
+    "revision": "1e29ef02ad48f238f818",
+    "url": "/FabioRelvas/projeto-imobiliaria-crud/static/js/main.f30aabe0.chunk.js"
+  },
+  {
+    "revision": "6e0c65b0a9e07c38ff16",
+    "url": "/FabioRelvas/projeto-imobiliaria-crud/static/js/runtime-main.1fff5095.js"
+  }
+]);
--- src/utils/data.js
+++
@@ -1,5 +0,0 @@
-const products = require('../data.json').imoveis;
-
-exports.findProducts = () => products;
-exports.findProduct = id =>
-	products.filter(product => product.id === parseInt(id))[0];
